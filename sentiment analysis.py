0d0b06bae4423c7a3c9c18d62d619bd8ff183c62
xmaseve <qiyi1224@gmail.com>
2016-03-12 23:40:42 -0500

    Added files via upload

---
+++ sentiment analysis.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 12 12:49:21 2016
+
+@author: YI
+"""
+
+import pandas as pd
+
+
+#read csv
+def read(a):
+    tweet=pd.read_csv(a)
+    date=tweet['time']
+    text=tweet['text']
+    day=pd.to_datetime(date,format='%Y-%m-%d')
+    day=day.map(lambda x: x.strftime('%Y-%m-%d'))
+    text = map(lambda x: [x], text)
+    tweet=pd.DataFrame(text,index=day,columns=['Text'])
+    return tweet.head()
+    
+tweet=read('amazon.csv')
+
+#filter data based on date
+sub226=tweet.loc[['2016-02-26']]
